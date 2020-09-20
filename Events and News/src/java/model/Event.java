4ed14579608cff1633e0b76a98067dc3478a3da3
enashfdo <enashf21@gmail.com>
2020-09-20 12:42:50 +0530

    added Event.java

---
+++ Events and News/src/java/model/Event.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Event {
+
+    private String id;
+    private String name;
+    private String details;
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getDetails() {
+        return details;
+    }
+
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    
+
+}
